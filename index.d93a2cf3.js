6217e734939074430e00bf6969a7fa3881a27989
Anya Laban <laban.anya.a@gmail.com>
2024-11-30 12:29:10 +0200

    make build

---
+++ index.d93a2cf3.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var c=t[a],l=document.createElement("li");l.textContent=a,r.append(l),Object.keys(c).length&&e(l,c)}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.d93a2cf3.js.map
